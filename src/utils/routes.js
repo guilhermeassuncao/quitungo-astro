b61b4a92ca428d183a3e10d008ae85e81951db6e
Guilherme Assunção <cgmatosassuncao@gmail.com>
2025-03-06 21:42:38 -0300

    feat: footer

---
+++ src/utils/routes.js
@@ -0,0 +1,11 @@
+export const routes = [
+    { slug: "trabalhos-academicos", title: "Trabalhos Acadêmicos" },
+    { slug: "cartografias", title: "Cartografias" },
+    { slug: "entrevistas-reportagens", title: "Entrevistas e Reportagens" },
+    { slug: "trabalhos-tecnicos", title: "Trabalhos Técnicos" },
+    { slug: "fotografias", title: "Fotografias" },
+    { slug: "literatura-teatro", title: "Literatura e Teatro" },
+    { slug: "cartilhas", title: "Cartilhas" },
+    { slug: "audiovisuais", title: "Audiovisuais" },
+    { slug: "artesanato-artes-plasticas", title: "Artesanato e Artes Plásticas" },
+  ];
