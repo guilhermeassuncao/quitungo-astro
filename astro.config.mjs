ed49b31bc07a6503fdff6533afec16dd97311b15
Guilherme Assunção <cgmatosassuncao@gmail.com>
2025-03-10 20:34:45 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro.guilhermeassuncao.dev/',
+    site: 'https://quitungo.guilhermeassuncao.dev',
 });
