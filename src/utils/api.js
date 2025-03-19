19ee33e95e80f2df02c54216a0fc49f070e41dfd
Guilherme Assunção <cgmatosassuncao@gmail.com>
2025-03-18 22:20:50 -0300

    feat: update

---
+++ src/utils/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = process.env.STRAPI_URL;
+const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
+
+
+export async function get(uri) {
+    try {
+        const { data } = await axios.get(`${BASE_URL}${uri}`, {
+            headers: {
+                Authorization: `Bearer ${STRAPI_TOKEN}`,
+            },
+        });
+
+        return data.data.map((item) => ({
+            slug: item.Rota,
+            title: item.Nome,
+            description: item.Descricao || '',
+        }));
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
