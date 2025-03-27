9392cfc4eac824a95daf7fc9678ddec3452fffec
Guilherme Assunção <cgmatosassuncao@gmail.com>
2025-03-27 20:53:31 -0300

    feat: update

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,12 +1,12 @@
 export const routes = [
   { slug: "quem-somos", title: "Quem Somos", description: "" },
   { slug: "trabalhos-academicos", title: "Trabalhos Acadêmicos", description: "Pesquisas e publicações" },
-  { slug: "cartografias", title: "Cartografias", description: "Mapas e outras representações geográficas" },
-  { slug: "entrevistas-reportagens", title: "Entrevistas e Reportagens", description: "Notícias e perspectivas acerca dos territórios quilombolas" },
   { slug: "trabalhos-tecnicos", title: "Trabalhos Técnicos", description: "Estudos e projetos realizados" },
-  { slug: "fotografias", title: "Fotografias", description: "Olhares acerca dos quilombos" },
-  { slug: "literatura-teatro", title: "Literatura e Teatro", description: "Textos literários e peças teatrais" },
+  { slug: "cartografias", title: "Cartografias", description: "Mapas e outras representações geográficas" },
   { slug: "cartilhas", title: "Cartilhas", description: "Materiais educativos" },
+  { slug: "fotografias", title: "Fotografias", description: "Olhares acerca dos quilombos" },
+  { slug: "entrevistas-reportagens", title: "Entrevistas e Reportagens", description: "Notícias e perspectivas acerca dos territórios quilombolas" },
   { slug: "audiovisuais", title: "Audiovisuais", description: "Filmes, documentários e outros registros" },
+  { slug: "literatura-teatro", title: "Literatura e Teatro", description: "Textos literários e peças teatrais" },
   { slug: "artesanato-artes-plasticas", title: "Artesanato e Artes Plásticas", description: "Trabalhos manuais e expressões artísticas visuais" }
 ];
