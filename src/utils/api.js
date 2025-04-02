86ba58b877e12def7dee1c2c122cac88249a68e3
Guilherme Assunção <cgmatosassuncao@gmail.com>
2025-04-01 21:47:53 -0300

    feat: updaet

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,9 +7,9 @@ const BASE_URL = process.env.STRAPI_URL;
 const STRAPI_TOKEN = process.env.STRAPI_TOKEN;
 
 
-export async function getCategories(uri) {
+export async function getCategories() {
     try {
-        const { data } = await axios.get(`${BASE_URL}${uri}`, {
+        const { data } = await axios.get(`${BASE_URL}/categorias`, {
             headers: {
                 Authorization: `Bearer ${STRAPI_TOKEN}`,
             },
@@ -20,6 +20,23 @@ export async function getCategories(uri) {
             title: item.Nome,
             description: item.Descricao || '',
         }));
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+export async function getPagesByCategory(categoryName) {
+    try {
+        const uri = `/paginas?populate=categorias&filters[categorias][Nome][$eq]=${encodeURIComponent(categoryName)}&populate=Capa.Imagem&populate=Conteudo.Imagem&populate=Conteudo.Video&populate=Conteudo.Documento&populate=Conteudo.Galeria.Imagem&populate=Conteudo.Audio&populate=Conteudo.Imagem.Imagem&populate=categorias&populate=Dados`;
+
+        const { data } = await axios.get(`${BASE_URL}${uri}`, {
+            headers: {
+                Authorization: `Bearer ${STRAPI_TOKEN}`,
+            },
+        });
+
+        return data.data;
     } catch (error) {
         console.error(error);
         return [];
